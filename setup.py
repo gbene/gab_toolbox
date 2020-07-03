e76390ca97d79d50af60d8a8631ef782d8e8bd74
gbene <40857804+gbene@users.noreply.github.com>
2020-07-04 00:05:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
       
       name = 'gab_toolbox',
       packages = ['gab_toolbox'],
-      version = '0.2.32' ,
+      version = '0.2.33' ,
       license = 'MIT',
       description = 'Functions that I recurrenlty use in other scripts.',
       author = 'Gabriele Benedetti',
       author_email = 'gabri.benedetti@gmail.com',
       url = 'https://github.com/gbene',
-      download_url = 'https://github.com//gbene/gab_toolbox/archive/v0.2.32.tar.gz' ,
+      download_url = 'https://github.com//gbene/gab_toolbox/archive/v0.2.33.tar.gz' ,
       keywords = ['formatting','files','opening','writing','conversions','latex','bibtex'],
       install_requires = [''],
       classifiers=[
